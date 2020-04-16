08f67198e7e4666786ff9ff606baf6209811125a
Drenight <2388201351@qq.com>
2020-04-16 20:47:32 +0800

    生语料查看

--- server.py
+++ server.py
@@ -12,14 +12,19 @@ class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render('index.html')
 
-class fileCountHandler(tornado.web.RequestHandler):
+class rawFileCountHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.write('3')
+		rawPath=os.getcwd()+"/raw"
+		rawFileCnt=os.listdir(rawPath)
+		self.write(str(rawFileCnt))
+
 
 if __name__ == '__main__':
 	tornado.options.parse_command_line()
 	app = tornado.web.Application(
-		handlers=[(r'/', IndexHandler), (r'/fileCount',fileCountHandler)],
+		handlers=[
+		(r'/', IndexHandler), 
+		(r'/rawFileCount',rawFileCountHandler)],
 	)
 	http_server = tornado.httpserver.HTTPServer(app)
 	http_server.listen(options.port)
